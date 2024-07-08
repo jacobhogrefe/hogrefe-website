882f7e4b594c74fe6fe5db0ff37e0ceeff92f25d
Jacob Hogrefe <jhogrefe03@gmail.com>
2024-07-08 15:36:29 -0400

    config changes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/hogrefe-website/' : '/',
-};
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/hogrefe-website/" : "/",
+});
